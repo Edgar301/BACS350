27180f6e6f8546e95a4f637e4f27acbf043959da
Edgar301 <46509883+Edgar301@users.noreply.github.com>
2021-09-10 22:04:24 -0600

    Url Fix

--- week3/Superhero/config/urls.py
+++ week3/Superhero/config/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from hero.views import BlackWidow, HulkView, IndexView, IronManView
 
 urlpatterns = [
-    path('index', IndexView.as_view()),
+    path('', IndexView.as_view()),
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
     path('blackwidow', BlackWidow.as_view()),
